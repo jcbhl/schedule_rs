b7085280c18af40e9737f36fd168be7e4b380d49
Jacob <jakehilliard01@gmail.com>
2021-01-17 18:46:02 -0500

    simplifying

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 // ['subject', 'catalog_number', 'class_section', 'class_number', 'class_title', 'class_topic_formal_desc', 'instructor', 'enrollment_capacity', 'meeting_days', 'meeting_time_start', 'meeting_time_end', 'term', 'term_desc']
-fn main(){
-    println!("Loading class data");
-    run();
-}
-async fn run(){
-    match get_class_data().await{
-        Ok(result) => println!(),
-        Err(error) => println!("error"),
-    }
-}
-async fn get_class_data() -> Result<String,reqwest::Error>{
+#[tokio::main]
+async fn main(){
+    println!("Fetching class data...");
     let url = "https://api.devhub.virginia.edu/v1/courses";
-    return reqwest::get(url).await?.text().await;
+    let response = reqwest::get(url).await;
+    match response {
+        Ok(result) => println!("Worked"),
+        Err(error) => return (),
+    }
+    println!("outside");
 }
