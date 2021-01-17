cabd2fa6e376464cf34c0223353d1ac95e074bfb
Jacob <jakehilliard01@gmail.com>
2021-01-17 18:29:32 -0500

    working?

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 // ['subject', 'catalog_number', 'class_section', 'class_number', 'class_title', 'class_topic_formal_desc', 'instructor', 'enrollment_capacity', 'meeting_days', 'meeting_time_start', 'meeting_time_end', 'term', 'term_desc']
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error>{
-    println!("Calling method...");
-    run_request().await;
-    Ok(())
+fn main(){
+    println!("Loading class data");
+    run();
 }
-async fn run_request() -> Result<(), reqwest::Error>{
-    let url = "https://api.devhub.virginia.edu/v1/courses";
-    match reqwest::get(url).await {
-        Ok(result) => println!("Responded with : {}", result.status()),
-        Err(error) => println!("Error caused by: {}", error),
-
+async fn run(){
+    match get_class_data().await{
+        Ok(result) => println!(),
+        Err(error) => println!("error"),
     }
-    //match reqwest::get(url).await {
-        //Ok(result) => println!("Responsed with : {}", result.status()),
-        //Err(error) => println!("Error caused by: {}", error),
-    //}
-    //let body = reqwest::get("https://api.devhub.virginia.edu/v1/courses").await?.text().await?;
-    // println!("{}",body);
-    Ok(())
+}
+async fn get_class_data() -> Result<String,reqwest::Error>{
+    let url = "https://api.devhub.virginia.edu/v1/courses";
+    return reqwest::get(url).await?.text().await;
 }
