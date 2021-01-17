7bfe965d36b5196eeccc34287d3c59d8052a2496
Jacob <jakehilliard01@gmail.com>
2021-01-17 16:57:30 -0500

    struggling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 // ['subject', 'catalog_number', 'class_section', 'class_number', 'class_title', 'class_topic_formal_desc', 'instructor', 'enrollment_capacity', 'meeting_days', 'meeting_time_start', 'meeting_time_end', 'term', 'term_desc']
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error>{
+    run_request();
+    Ok(())
+}
+async fn run_request() -> Result<(), reqwest::Error>{
+    let _url = "https://api.devhub.virginia.edu/v1/courses";
+    //match reqwest::get(url).await {
+        //Ok(result) => println!("Responsed with : {}", result.status()),
+        //Err(error) => println!("Error caused by: {}", error),
+    //}
+    let response = reqwest::get("https://api.devhub.virginia.edu/v1/courses").await?.text().await?;
+    Ok(())
 }
