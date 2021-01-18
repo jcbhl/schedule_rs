3004294c6d6e6da6fd448acc6ee1e36ced01832d
Jacob <jakehilliard01@gmail.com>
2021-01-18 10:40:36 -0500

    minimum possible code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 // ['subject', 'catalog_number', 'class_section', 'class_number', 'class_title', 'class_topic_formal_desc', 'instructor', 'enrollment_capacity', 'meeting_days', 'meeting_time_start', 'meeting_time_end', 'term', 'term_desc']
 #[tokio::main]
-async fn main(){
+async fn main() -> Result<(), reqwest::Error>{
     println!("Fetching class data...");
     let url = "https://api.devhub.virginia.edu/v1/courses";
-    let response = reqwest::get(url).await;
-    match response {
-        Ok(result) => println!("Worked"),
-        Err(error) => return (),
-    }
-    println!("outside");
+    let response = reqwest::get(url).await?.text().await?;
+    println!("{}",response);
+    return Ok(());
 }
