d67198333b833e78f79d7b1848a1fe52b73c584b
Jacob <jakehilliard01@gmail.com>
2021-01-26 21:02:59 -0500

    Learning how to read from stdin. Other projects are spawning subprocesses but this should work for now.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // ['subject', 'catalog_number', 'class_section', 'class_number', 'class_title', 'class_topic_formal_desc', 'instructor', 'enrollment_capacity', 'meeting_days', 'meeting_time_start', 'meeting_time_end', 'term', 'term_desc']
-//consider fuzzy finding  
 use structopt::StructOpt;
+use std::io;
 
 mod class;
 
 #[derive(StructOpt)]
-struct Command{
+struct Cli{
     desired_class_title: String,
     // class_type: ClassTypes,
 }
@@ -19,28 +19,36 @@ async fn request_and_parse() -> Result<json::JsonValue, Box<dyn std::error::Erro
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //todo cache these results or allow for longer-running program - api call time sucks UX wise
-    let parsed = request_and_parse().await?;
-    let _column_labels = parsed["class_schedules"]["columns"].members();
-    let data = parsed["class_schedules"]["records"].members();
+    // //todo cache these results on disk
+    // let parsed = request_and_parse().await?;
+    // let _column_labels = parsed["class_schedules"]["columns"].members();
+    // let data = parsed["class_schedules"]["records"].members();
 
-    let mut class_vec: Vec<class::Class> = Vec::new();
-    for row in data {
-        let current_class = class::Class::build_from_json(row);
-        class_vec.push(current_class);
-    }
-    let args = Command::from_args();
-    println!("Searching for all classes matching {}", &args.desired_class_title);
-    let mut matching_vec = Vec::new();
-    for class in class_vec{
-        if class.class_title.contains(&args.desired_class_title){
-            matching_vec.push(class);
+    // let mut class_vec: Vec<class::Class> = Vec::new();
+    // for row in data {
+    //     let current_class = class::Class::build_from_json(row);
+    //     class_vec.push(current_class);
+    // }
+    // let args = Cli::from_args();
+    // println!("Searching for all classes matching {}", &args.desired_class_title);
+    // let mut matching_vec = Vec::new();
+    // for class in class_vec{
+    //     if class.class_title.contains(&args.desired_class_title){
+    //         matching_vec.push(class);
+    //     }
+    // }
+    // println!("Found {} classes that match... Printing", matching_vec.len());
+    // for matched_class in matching_vec{
+    //     println!("{:?}", matched_class);
+    // }
+    loop{
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input){
+            Ok(_) => println!("Hello {}", &input),
+            Err(error) => println!("Error {}", error),
         }
     }
-    println!("Found {} classes that match... Printing", matching_vec.len());
-    for matched_class in matching_vec{
-        println!("{:?}", matched_class);
-    }
-
     Ok(())
 }
+
+
